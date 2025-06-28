db1c8783a1594099c468ffac24d4aedb858c41f6
Lance <149887891+lance-ui@users.noreply.github.com>
2025-06-28 17:04:24 +0200

    update handler/handleOnReply.js

--- handler/handleOnReply.js
+++ handler/handleOnReply.js
@@ -1,17 +1,31 @@
 export default async ({ sock, event, threadID, senderID, proto }) => {
-  const { replies, commands } = global.client;
-  try{
-    if(replies.has(event.message.extendedTextMessage?.contextInfo.stanzaId)){
-      const data = replies.get(event.message.extendedTextMessage?.contextInfo.stanzaId)
-      if(data.commandName){
-        const command = commands.get(data.commandName)
-        if(command.onReply){
-          await command.onReply({ sock, threadID, senderID, proto, font, message, bot})
+    const { replies, commands } = global.client;
+    try {
+        if (
+            replies.has(event.message.extendedTextMessage?.contextInfo.stanzaId)
+        ) {
+            const { commandName, ...data} = replies.get(
+                event.message.extendedTextMessage?.contextInfo.stanzaId
+            );
+            if (commandName) {
+                const command = commands.get(commandName);
+                if (command.onReply) {
+                    await command.onReply({
+                        sock,
+                        threadID,
+                        senderID,
+                        proto,
+                        font,
+                        message,
+                        bot
+                    });
+                }
+            }
         }
-      }
+    } catch (err) {
+        console.log(err.message);
+        message.reply(
+            "failed to handle onReply please contact admin if u can so tht this is fixed immediately"
+        );
     }
-  } catch (err){
-    console.log(err.message)
-    message.reply("failed to handle onReply please contact admin if u can so tht this is fixed immediately")
-  }
-}
+};
