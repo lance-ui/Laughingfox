b6a1ec0d0cd23122310d3aa7403565a96eb31f05
Lance <149887891+lance-ui@users.noreply.github.com>
2025-07-01 23:10:35 +0200

    Add files via upload

--- handler/handleOnReply.js
+++ handler/handleOnReply.js
@@ -1,15 +1,40 @@
-export default async ({ sock, event, threadID, senderID, proto }) => {
+function getStanzaId(message) {
+    if (
+        message.extendedTextMessage &&
+        message.extendedTextMessage.contextInfo
+    ) {
+        return message.extendedTextMessage.contextInfo.stanzaId;
+    } else if (message.imageMessage && message.imageMessage.contextInfo) {
+        return message.imageMessage.contextInfo.stanzaId;
+    } else if (message.stickerMessage && message.stickerMessage.contextInfo) {
+        return message.stickerMessage.contextInfo.stanzaId;
+    } else if (message.videoMessage && message.videoMessage.contextInfo) {
+        return message.videoMessage.contextInfo.stanzaId;
+    } else if (message.contextInfo) {
+        return message.contextInfo.stanzaId;
+    }
+    return null;
+}
+
+export default async ({
+    sock,
+    event,
+    threadID,
+    senderID,
+    proto,
+    font,
+    message,
+    bot,
+    args
+}) => {
     const { replies, commands } = global.client;
     try {
-        if (
-            replies.has(event.message.extendedTextMessage?.contextInfo.stanzaId)
-        ) {
-            const { commandName, ...data} = replies.get(
-                event.message.extendedTextMessage?.contextInfo.stanzaId
-            );
-            if (commandName) {
-                const command = commands.get(commandName);
-                if (command.onReply) {
+        const stanzaId = getStanzaId(event.message);
+        if (stanzaId && replies.has(stanzaId)) {
+            const data = replies.get(stanzaId);
+            if (data && data.commandName) {
+                const command = commands.get(data.commandName);
+                if (command && command.onReply) {
                     await command.onReply({
                         sock,
                         threadID,
@@ -17,15 +42,17 @@ export default async ({ sock, event, threadID, senderID, proto }) => {
                         proto,
                         font,
                         message,
-                        bot
+                        bot,
+                        args,
+                        data
                     });
                 }
             }
         }
     } catch (err) {
-        console.log(err.message);
+        console.log(err);
         message.reply(
-            "failed to handle onReply please contact admin if u can so tht this is fixed immediately"
+            "Failed to handle onReply. Please contact admin so that this is fixed immediately."
         );
     }
 };
--- handler/messagehandler.js
+++ handler/messagehandler.js
@@ -1,180 +1,367 @@
-import commandHander from "./commandhandler.js";
+import commandHander from "./commandHandler.js";
+import path from "path";
 import handleOnReply from "./handleOnReply.js";
+import { dirname } from "path";
+
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
 export default async ({ font, sock, event, log, proto }) => {
+    let senderID;
     try {
+
         const threadID = event.key.remoteJid;
-        const senderID = event.key.participant;
+
+        senderID = event.key.participant;
+        if(!senderID) {
+           senderID = threadID.split("@")[0] + "@lid" 
+        }
+
         const message = {
+
             send: async form => {
+
                 return await sock.sendMessage(threadID, { text: form });
+
             },
+
             reply: async form => {
+
                 return await sock.sendMessage(
+
                     threadID,
+
                     { text: form },
+
                     { quoted: event }
+
                 );
+
             },
+
             edit: async (form, data) => {
+
                 return await sock.sendMessage(threadID, {
+
                     text: form,
+
                     edit: data.key
+
                 });
+
             },
+
             react: async (emoji, data) => {
+
                 return await sock.sendMessage(threadID, {
+
                     react: {
+
                         text: emoji,
+
                         key: data.key
+
                     }
+
                 });
+
             },
+
             unsend: async data => {
+
                 await sock.sendMessage(threadID, { delete: data.key });
+
             },
+
             sendGif: async (filepath, cap) => {
+
                 return await sock.sendMessage(threadID, {
+
                     video: {
+
                         url: filepath,
+
                         caption: cap || "",
+
                         gifPlayback: true
+
                     }
+
                 });
+
             },
+
             sendAudio: async (filepath, cap) => {
+
                 return await sock.sendMessage(threadID, {
+
                     audio: {
+
                         url: filepath,
+
                         caption: cap || ""
+
                     }
+
                 });
+
             },
+
             sendVideo: async (cap, filepath, boo) => {
+
                 return await sock.sendMessage(threadID, {
+
                     image: {
+
                         url: filepath
+
                     },
+
                     viewOnce: boo || false,
+
                     caption: cap || ""
+
                 });
+
             },
+
             sendImage: async (cap, filepath, boo) => {
+
                 return await sock.sendMessage(threadID, {
+
                     image: {
+
                         url: filepath
+
                     },
+
                     viewOnce: boo || false,
+
                     caption: cap || ""
+
                 });
+
             }
+
         };
+
         const bot = {
+
             changeProfileStatus: async form => {
+
                 return await sock.updateProfileStatus(form);
+
             },
+
             changeProfileName: async form => {
+
                 return await sock.updateProfileName(form);
+
             },
+
             changeProfilePic: async filepath => {
+
                 return await sock.updateProfilePicture(threadID, {
+
                     url: filepath
+
                 });
+
             },
+
             removeProfilePic: async id => {
+
                 return await sock.removeProfilePicture(id);
+
             },
+
             createGroup: async (sock, name, members) => {
+
                 return sock.groupCreate(name, [members]);
+
             },
+
             participants: async (id, action) => {
+
                 return await sock.groupParticipantsUpdate(
+
                     threadID,
+
                     [id],
+
                     action
+
                 );
+
             },
+
             leave: async id => {
+
                 return await sock.groupLeave(id);
+
             },
+
             user: async (id, action) => {
+
                 return await sock.updateBlockStatus(id, action);
+
             }
+
         };
+
         if (!event.message) return;
+
         let args = "";
 
         if (event.message.conversation) {
+
             args = event.message.conversation;
+
         } else if (event.extendedTextMessage) {
+
             args = event.extendedTextMessage.text;
+
         } else if (event.imageMessage) {
+
             args = event.imageMessage.caption || "";
+
         } else if (event.videoMessage) {
+
             args = event.videoMessage.caption || "";
+
         } else if (event.message.extendedTextMessage) {
+
             args = event.message.extendedTextMessage.text;
+
         }
-        const isPrefixed = args.startsWith(global.client.config.PREFIX);
-        if (
-            !global.client.config.admins.includes(
-                senderID.replace("@lid", "")
-            ) &&
-            global.client.config.private &&
-            isPrefixed
-        ) {
-            return message.send(
-                `❌  | currently only the bot admin can use the bot`
-            );
-        }
-        if (args.toLowerCase() == "prefix") {
-            const form = `◣✦◥▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔◤✦◢
-               𝗕𝗢𝗧 𝗣𝗥𝗘𝗙𝗜𝗫•[${global.client.config.PREFIX}]
-◤✦◢▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁◣✦◥`;
-            const datapath = await path.join(
-                __dirname,
-                "..",
-                "cache",
-                "tmp",
-                "0bcb6c5caa664b982dd49d18aca40941.jpg"
-            );
-            return await message.sendImage(form, datapath);
-        }
-        if (!isPrefixed) return;
-        const [commandName, ...commandArgs] = args
-            .slice(global.client.config.PREFIX.length)
-            .trim()
-            .split(" ");
 
         await handleOnReply({
+
             sock,
+
             event,
+
             threadID,
+
             senderID,
-            args: commandArgs,
-            proto
+
+            proto,
+
+            event,
+
+            font,
+
+            bot,
+
+            message,
+            
+            args
+
         });
 
-        if (!global.client.commands.has(commandName.toLowerCase())) {
-            await sock.sendMessage(threadID, {
-                text: `❌ |  the  command '${commandName}' does not exist type ${global.client.config.PREFIX}help to view all the available commands`
-            });
-            return;
+        const isPrefixed = args.startsWith(global.client.config.PREFIX);
+
+        if (
+
+            !global.client.config.admins.includes(
+
+                senderID.replace("@lid", "")
+
+            ) &&
+
+            global.client.config.private &&
+
+            isPrefixed
+
+        ) {
+
+            return message.send(
+
+                `❌  | currently only the bot admin can use the bot`
+
+            );
+
         }
+
+        if (args.toLowerCase() == "prefix") {
+
+            const form = `◣✦◥▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔◤✦◢
+
+                   𝗕𝗢𝗧 𝗣𝗥𝗘𝗙𝗜𝗫•[${global.client.config.PREFIX}]
+
+◤✦◢▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁◣✦◥`;
+
+            const datapath = await path.join(
+
+                __dirname,
+
+                "..",
+
+                "cache",
+
+                "tmp",
+
+                "0bcb6c5caa664b982dd49d18aca40941.jpg"
+
+            );
+
+            return await message.sendImage(form, datapath);
+
+        }
+
+        if (!isPrefixed) return;
+
+        const [commandName, ...commandArgs] = args
+
+            .slice(global.client.config.PREFIX.length)
+
+            .trim()
+
+            .split(" ");
+
+        if (!global.client.commands.has(commandName.toLowerCase())) {
+
+            await sock.sendMessage(threadID, {
+
+                text: `❌ |  the  command '${commandName}' does not exist type ${global.client.config.PREFIX}help to view all the available commands`
+
+            });
+
+            return;
+
+        }
+
         await commandHander({
+
             sock,
+
             event,
+
             threadID,
+
             senderID,
+
             args: commandArgs,
+
             log,
+
             commandName,
+
             font,
+
             message,
+
             bot,
+
             proto
+
         });
+
     } catch (e) {
+
         console.log(e);
+
     }
+
 };
