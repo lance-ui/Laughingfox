e14687303702fb1a06611a7f139588d2327c6149
Lance <149887891+lance-ui@users.noreply.github.com>
2025-08-26 12:46:26 +0200

    update utils/utils.js

--- utils/utils.js
+++ utils/utils.js
@@ -5,154 +5,111 @@ import path, { dirname } from "path";
 import fs from "fs-extra";
 import { fileURLToPath } from "url";
 import font from "./fonts.js";
-import schedule from "node-schedule";
-import { createRequire } from "module";
-const require = createRequire(import.meta.url);
+import axios from "axios";
+import FormData from "form-data";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-async function loadCommands() {
-    Object.keys(require.cache).forEach(key => delete require.cache[key]);
+class Utils {
+  async loadCommands() {
     const errs = {};
     const commandsPath = path.join(__dirname, "..", "scripts", "cmds");
-
     try {
-        log.info("loading commands");
-        const commandFiles =
-            fs.readdirSync(commandsPath).filter(file => file.endsWith(".js")) ||
-            file.endsWith(".ts");
-
-        for (const file of commandFiles) {
-            try {
-                const commandModule = await import(
-                    path.join(commandsPath, file)
-                );
-                const command = commandModule.default;
-                if (!command) {
-                    throw new Error(`Error: ${file} does not export anything!`);
-                } else if (!command.config) {
-                    throw new Error(`Error: ${file} does not export config!`);
-                } else {
-                    if (
-                        command.config.name.includes(
-                            global.client.config.unloadedCmds
-                        )
-                    ) {
-                        continue;
-                    }
-                    global.client.commands.set(command.config.name, command);
-                    if (command.config.aliase) {
-                        for (const alias of command.config.aliase) {
-                            global.client.aliases.set(alias, command);
-                        }
-                    } else if (command.config.cooldown) {
-                        global.client.cooldowns.set(
-                            command.config.cooldown,
-                            []
-                        );
-                    }
-                    log.success(`${command.config.name} successfully loaded`);
-                }
-            } catch (error) {
-                log.error(`Error loading command ${file}: ${error.message}`);
+      log.info("loading commands");
+      const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith(".js"));
+      for (const file of commandFiles) {
+        try {
+          const filePath = path.join(commandsPath, file);
+          const commandModule = await import(filePath);
+          const command = commandModule.default;
+          if (!command) {
+            throw new Error(`Error: ${file} does not export anything!`);
+          } else if (!command.config) {
+            throw new Error(`Error: ${file} does not export config!`);
+          } else {
+            if (command.config.name.includes(global.client.config.unloadedCmds)) {
+              continue;
             }
+            global.client.commands.set(command.config.name, command);
+            if (command.config.aliase) {
+              for (const alias of command.config.aliase) {
+                global.client.aliases.set(alias, command);
+              }
+            } else if (command.config.cooldown) {
+              global.client.cooldowns.set(command.config.cooldown, []);
+            }
+            log.success(`${command.config.name} successfully loaded`);
+          }
+        } catch (error) {
+          log.error(`Error loading command ${file}: ${error.message}`);
         }
+      }
     } catch (error) {
-        log.error(error.message);
+      log.error(error.message);
     }
     return Object.keys(errs).length === 0 ? false : errs;
-}
-async function loadEvents() {
-    Object.keys(require.cache).forEach(key => delete require.cache[key]);
+  }
+
+  async loadEvents() {
     log.info("loading events");
-    const __dirname = dirname(fileURLToPath(import.meta.url));
     const eventsPath = path.join(__dirname, "..", "scripts", "events");
-    const eventFiles = fs
-        .readdirSync(eventsPath)
-        .filter(file => file.endsWith(".js"));
-
+    const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith(".js"));
     for (const file of eventFiles) {
-        try {
-            const eventModule = await import(path.join(eventsPath, file));
-            const event = eventModule.default;
-            if (!event) {
-                throw new Error(`Error: ${file} does not export anything!`);
-            } else if (!event.config) {
-                throw new Error(`Error: ${file} does not export config`);
-            } else if (!event.onEvent) {
-                throw new Error(`Error: ${file} does not export onEvent!`);
-            } else {
-                global.client.events.set(event.config.name, event);
-                log.success(`${event.config.name} successfully loaded`);
-            }
-        } catch (error) {
-            log.error(`Error loading event ${file}: ${error.message}`);
+      try {
+        const filePath = path.join(eventsPath, file);
+        const eventModule = await import(filePath);
+        const event = eventModule.default;
+        if (!event) {
+          throw new Error(`Error: ${file} does not export anything!`);
+        } else if (!event.config) {
+          throw new Error(`Error: ${file} does not export config`);
+        } else if (!event.onEvent) {
+          throw new Error(`Error: ${file} does not export onEvent!`);
+        } else {
+          global.client.events.set(event.config.name, event);
+          log.success(`${event.config.name} successfully loaded`);
         }
+      } catch (error) {
+        log.error(`Error loading event ${file}: ${error.message}`);
+      }
     }
-}
+  }
 
-const loadAll = async () => {
-    await loadCommands();
-    await loadEvents();
-};
-setTimeout(loadAll, 5000);
+  async loadAll() {
+    await this.loadCommands();
+    await this.loadEvents();
+  }
 
-async function saveCreds(creds) {
+  async saveCreds(creds) {
     try {
-        const sessionDir = path.join(
-            __dirname,
-            "..",
-            "cache",
-            "auth_info_baileys"
-        );
-        await fs.writeFile(
-            path.join(sessionDir, "creds.json"),
-            JSON.stringify(creds)
-        );
-        log.info("Authentication credentials saved successfully");
+      const sessionDir = path.join(__dirname, "..", "cache", "auth_info_baileys");
+      await fs.writeFile(path.join(sessionDir, "creds.json"), JSON.stringify(creds));
+      log.info("Authentication credentials saved successfully");
     } catch (error) {
-        log.error("Error saving authentication credentials:", error);
+      log.error("Error saving authentication credentials:", error);
     }
-}
+  }
 
-async function removeFiles(filePath, time, callback) {
-    if (!filePath) return;
-    const fileName = path.basename(filePath);
-    const rule = new schedule.RecurrenceRule();
-    if (time.includes("h")) {
-        const hours = parseInt(time.replace("h", ""));
-        rule.hour = new schedule.Range(0, 23, hours);
-    } else if (time.includes("m")) {
-        const minutes = parseInt(time.replace("m", ""));
-        rule.minute = new schedule.Range(0, 59, minutes);
-    }
-
-    schedule.scheduleJob(rule, async () => {
-        try {
-            if (await fs.pathExists(filePath)) {
-                await fs.unlink(filePath);
-                log.info(`Removed file: ${filePath}`);
-                if (callback) callback(null, `File removed: ${filePath}`);
-            } else {
-                log.info(`File not found: ${filePath}`);
-                if (callback)
-                    callback(new Error(`File not found: ${filePath}`));
-            }
-        } catch (error) {
-            log.error(`Error removing file: ${error.message}`);
-            if (callback) callback(error);
+  async uploadToImgbb(filePath) {
+    try {
+      const formData = new FormData();
+      formData.append('image', fs.createReadStream(filePath));
+      const response = await axios.post(`https//api.imgbb.com/1/upload?key=${global.client.config.keys.IMGBB}`, formData, {
+        headers: {
+          ...formData.getHeaders()
         }
-    });
+      });
+      return response.data.data;
+    } catch (error) {
+      log.error(`Error uploading file to imgbb: ${error.message}`);
+    }
+  }
 
-    log.info(`Job started. Removing ${fileName} ${time}.`);
+  get font() {
+    return font;
+  }
 }
 
-const utils = {
-    loadAll,
-    loadCommands,
-    loadEvents,
-    font,
-    saveCreds,
-    removeFiles
-};
+const utils = new Utils();
+setTimeout(utils.loadAll.bind(utils),5000)
 export default utils;
